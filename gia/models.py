3f0c7acbad30527610c9db5e143035c232c1769a
pikhaev1988 <100423743+pikhaev1988@users.noreply.github.com>
2023-04-28 10:54:40 +0300

    baza

--- gia/models.py
+++ gia/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class mod(models.Model):
     im = models.ImageField(upload_to='media', verbose_name='Фото')
 
+class name(models.Model):
+    nam  = models.CharField(max_length=100)
--- gia_urus/settings.py
+++ gia_urus/settings.py
@@ -68,8 +68,11 @@ WSGI_APPLICATION = 'gia_urus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'gia_urus',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '19880707_Ap',
+        'HOST': 'localhost',
     }
 }
 
