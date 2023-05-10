8bf5ac6b4152844764cc6e02ff4a0e83b453b2e5
pikhaev1988 <100423743+pikhaev1988@users.noreply.github.com>
2023-05-10 10:32:25 +0300

    url

--- gia_urus/urls.py
+++ gia_urus/urls.py
@@ -2,10 +2,27 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve as mediaserve
+from django.urls import re_path as url
 
 urlpatterns = [
     path('', include('gia.url')),
     path('admin/', admin.site.urls),
 ]
+
 if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+else:
+    urlpatterns += [
+        url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.MEDIA_ROOT}),
+        url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.STATIC_ROOT}),
+    ]
